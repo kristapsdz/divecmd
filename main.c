0de5d04f14b177f1a1a69c93cc57ff0d4690d9e4
kristaps <>
2017-01-03 11:24:14 +0000

    Finalise fingerprint file read and suppress write with -n.

--- main.c
+++ main.c
@@ -26,6 +26,7 @@
 #include <assert.h>
 #include <ctype.h>
 #include <err.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <signal.h>
@@ -206,8 +207,32 @@ hex2bin(const char *str)
 	return buffer;
 }
 
+static void
+fingerprint_set(int fd, const char *file, dc_buffer_t *buf)
+{
+	ssize_t	 ssz;
+
+	if (-1 == ftruncate(fd, 0))
+		err(EXIT_FAILURE, "%s", file);
+	ssz = write(fd, 
+		dc_buffer_get_data(buf),
+		dc_buffer_get_size(buf));
+	if (-1 == ssz)
+		err(EXIT_FAILURE, "%s", file);
+	else if ((size_t)ssz != dc_buffer_get_size(buf))
+		errx(EXIT_FAILURE, "%s: short write", file);
+}
+
+/*
+ * Get the last-known fingerprint of "device".
+ * Sets "fd" and "filep" to be the file descriptor and filename of the
+ * fingerprint file, or -1 and NULL.
+ * If device is NULL, nothing happens.
+ * If the file was empty (e.g., just created), the return is NULL, but
+ * the file is open anyway.
+ */
 static dc_buffer_t *
-fingerprint_get(int *fd, const char *device)
+fingerprint_get(int *fd, char **filep, const char *device)
 {
 	char		*cp, *ccp, *file;
 	char		 buf[1024];
@@ -215,11 +240,33 @@ fingerprint_get(int *fd, const char *device)
 	struct stat	 st;
 	size_t		 sz;
 	ssize_t		 ssz;
+	int		 rc;
 
 	*fd = -1;
+	*filep = NULL;
 
 	if (NULL == device)
 		return(NULL);
+
+	/*
+	 * First, make sure our directory exists.
+	 * If it doesn't, create it.
+	 */
+
+	if (asprintf(&file, "%s/.divecmd", getenv("HOME")) < 0)
+		err(EXIT_FAILURE, NULL);
+	if (-1 == (rc = stat(file, &st)) && ENOENT != errno)
+		err(EXIT_FAILURE, "%s", file);
+
+	if (-1 == rc) {
+		if (verbose)
+			fprintf(stderr, "%s: creating\n", file);
+		if (-1 == mkdir(file, 0755))
+			err(EXIT_FAILURE, "%s", file);
+	} else if ( ! (S_IFDIR & st.st_mode)) 
+		errx(EXIT_FAILURE, "%s: not a directory", file);
+
+	free(file);
 
 	/* Escape device name as file. */
 
@@ -229,19 +276,29 @@ fingerprint_get(int *fd, const char *device)
 		if (isspace((int)*ccp) || '(' == *ccp || ')' == *ccp)
 			*ccp = '_';
 
-	/* Open ~/.divecmd/DEVICE read-only, exiting on fail. */
+	/* Open ~/.divecmd/DEVICE, exiting on new file or fail. */
 
 	if (asprintf(&file, "%s/.divecmd/%s", getenv("HOME"), cp) < 0)
 		err(EXIT_FAILURE, NULL);
-	if (-1 == (*fd = open(file, O_RDONLY, 0)))
-		goto out;
+
+	*filep = file;
+
+	if (-1 == (*fd = open(file, O_CREAT|O_RDWR, 0644)))
+		err(EXIT_FAILURE, "%s", file);
 	else if (-1 == fstat(*fd, &st))
 		err(EXIT_FAILURE, "%s", file);
+
+	/* Empty file: we just created it? */
+
+	if (0 == st.st_size) {
+		if (verbose)
+			fprintf(stderr, "%s: creating\n", file);
+		goto out;
+	}
 
 	/* Copy the entire file (fingerprint) into buffer. */
 
 	sz = st.st_size;
-
 	if (NULL == (res = dc_buffer_new(sz)))
 		err(EXIT_FAILURE, NULL);
 
@@ -256,36 +313,36 @@ fingerprint_get(int *fd, const char *device)
 
 	/* Clean up descriptors and temporary memory. */
 out:
-	if (NULL != res && verbose > 0)
+	if (NULL != res && verbose)
 		fprintf(stderr, "%s: good fingerprint\n", file);
-	if (NULL == res && verbose > 0)
+	if (NULL == res && verbose)
 		fprintf(stderr, "%s: no fingerprint\n", file);
 		
 	free(cp);
-	free(file);
 	return(res);
 }
 
 int
 main(int argc, char *argv[])
 {
-	int		 exitcode = EXIT_FAILURE;
+	int		 exitcode = 0;
 	dc_status_t	 status = DC_STATUS_SUCCESS;
 	dc_context_t	*context = NULL;
 	dc_descriptor_t *descriptor = NULL;
 	dc_loglevel_t 	 loglevel = DC_LOGLEVEL_WARNING;
 	const char 	*device = NULL, *ofp = NULL;
 	const char	*udev = "/dev/ttyU0";
-	int 		 show = 0, ch, ofd = -1;
+	int 		 show = 0, ch, ofd = -1, nofp = 0;
 	dc_buffer_t	*fprint = NULL, *ofprint = NULL, *lprint = NULL;
 	enum dcmd_type	 out = DC_OUTPUT_XML;
+	char		*ofile = NULL;
 
 #if defined(__OpenBSD__) && OpenBSD > 201510
 	if (-1 == pledge("stdio rpath", NULL))
 		err(EXIT_FAILURE, "pledge");
 #endif
 
-	while (-1 != (ch = getopt (argc, argv, "d:f:lsv"))) {
+	while (-1 != (ch = getopt (argc, argv, "d:f:lnsv"))) {
 		switch (ch) {
 		case 'd':
 			device = optarg;
@@ -295,6 +352,9 @@ main(int argc, char *argv[])
 			break;
 		case 'l':
 			out = DC_OUTPUT_LIST;
+			break;
+		case 'n':
+			nofp = 1;
 			break;
 		case 's':
 			show = 1;
@@ -317,16 +377,21 @@ main(int argc, char *argv[])
 	else if (0 == show && argc)
 		udev = argv[0];
 
-	/* Convert fingerprints into binary. */
+	/* 
+	 * Convert desired fingerprint into binary.
+	 * Also read in the last-seen fingerprint.
+	 * We'll [optionally] use these to constrain which dives we
+	 * report to the operator.
+	 */
 
 	if (NULL != ofp)
 		ofprint = hex2bin(ofp);
 
-	fprint = fingerprint_get(&ofd, device);
+	fprint = fingerprint_get(&ofd, &ofile, device);
 
 	/* Setup the cancel signal handler. */
 
-	signal (SIGINT, sighandler);
+	signal(SIGINT, sighandler);
 
 	/* Initialize a library context. */
 
@@ -342,8 +407,7 @@ main(int argc, char *argv[])
 	dc_context_set_logfunc(context, logfunc, NULL);
 
 	if (0 == show) {
-		status = search_descr
-			(&descriptor, device);
+		status = search_descr(&descriptor, device);
 		if (status != DC_STATUS_SUCCESS) {
 			warnx("%s", dctool_errmsg(status));
 			goto cleanup;
@@ -354,14 +418,30 @@ main(int argc, char *argv[])
 	}
 
 	if (show) {
+		/* We're only showing devices. */
 		show_devices();
-		exitcode = EXIT_SUCCESS;
+		exitcode = 1;
 		goto cleanup;
 	} 
+
+	/*
+	 * Do the full download and parse, setting the last fingerprint
+	 * if it's found.
+	 */
 
 	exitcode = download
 		(context, descriptor, udev, 
 		 out, fprint, ofprint, &lprint);
+
+	/* 
+	 * If we have a last fingerprint, write it. 
+	 * We do this by default, but it can be turned off: the operator
+	 * says so (-n), the parse failed, or no fingerprint was parsed.
+	 */
+
+	if (0 == nofp && exitcode && NULL != lprint && -1 != ofd)
+		fingerprint_set(ofd, ofile, lprint);
+
 cleanup:
 	if (-1 != ofd)
 		close(ofd);
@@ -369,9 +449,10 @@ cleanup:
 	dc_context_free(context);
 	dc_buffer_free(lprint);
 	free(fprint);
-	return(exitcode);
+	free(ofile);
+	return(exitcode ? EXIT_SUCCESS : EXIT_FAILURE);
 usage:
-	fprintf(stderr, "usage: %s [-v] [-d computer] [-f fingerprint] [device]\n"
+	fprintf(stderr, "usage: %s [-nv] [-d computer] [-f fingerprint] [device]\n"
 			"       %s [-v] -s\n",
 			getprogname(), getprogname());
 	return(EXIT_FAILURE);
