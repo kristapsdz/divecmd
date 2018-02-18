28d062498bd0f5d9b390f24cb8d0d66510a415fa
kristaps <>
2018-02-18 09:40:42 +0000

    Show parse identifier.

--- divecmd2list.c
+++ divecmd2list.c
@@ -62,7 +62,7 @@ print_datetime(const struct dive *d)
 {
 	struct tm	*tm = localtime(&d->datetime);
 
-	printf("  %04d-%02d-%02d %02d:%02d:%02d  ",
+	printf("%04d-%02d-%02d %02d:%02d:%02d  ",
 		tm->tm_year + 1900, tm->tm_mon + 1, 
 		tm->tm_mday, tm->tm_hour, tm->tm_min, 
 		tm->tm_sec);
@@ -111,6 +111,7 @@ print_all(int human, const struct divestat *ds)
 					temp += s->temp;
 					tempsz++;
 				}
+			printf("  %5zu  ", d->pid);
 			print_datetime(d);
 			printf("%5.2f  ", d->maxdepth);
 			if (tempsz > 0)
